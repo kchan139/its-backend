78ccd7ade2bc086b714fe4833769f91bc7faa38a
Nguyen Ho Phi Ung <phiung23122004@gmail.com>
2025-12-06 23:02:24 +0700

    fix: change file names

--- identity-service/internal/repositories/user_repository.go
+++ identity-service/internal/repositories/user_repository.go
@@ -1,75 +1,10 @@
 package repositories
 
-import (
-	"errors"
+import "github.com/kchan139/intelligent-tutoring-system/identity-service/internal/models"
 
-	"github.com/kchan139/intelligent-tutoring-system/identity-service/internal/models"
-	"github.com/kchan139/intelligent-tutoring-system/identity-service/internal/utils"
-	"gorm.io/gorm"
-)
-
-type UserRepository struct {
-	db *gorm.DB
-}
-
-func NewUserRepository(db *gorm.DB) *UserRepository {
-	return &UserRepository{db}
-}
-
-func (r *UserRepository) FindByEmail(email string) (*models.User, error) {
-	var user models.User
-	result := r.db.Where("email = ?", email).First(&user)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return &user, nil
-}
-
-func (r *UserRepository) StoreUser(email, fullname, hashed_password, role string) error {
-	var dbRole models.Role
-	result := r.db.Where("role_name = ?", role).First(&dbRole)
-	if result.Error != nil {
-		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			return errors.New("invalid role: role does not exist")
-		}
-		return result.Error
-	}
-	user := models.User{
-		Email:    email,
-		Fullname: fullname,
-		Password: string(hashed_password),
-		Avatar:   utils.GenerateAvatar(fullname),
-		RoleID:   &dbRole.ID, // Assign pointer to roleID
-	}
-	if err := r.db.Create(&user).Error; err != nil {
-		return err
-	}
-	return nil
-}
-
-func (r *UserRepository) CheckUser(email, password string) (*models.User, error) {
-	var user models.User
-	result := r.db.Preload("Role").Where("email = ?", email).First(&user)
-	if result.Error != nil {
-		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			return nil, errors.New("invalid credentials")
-		}
-		return nil, result.Error
-	}
-	valid := utils.CheckPassword(password, user.Password)
-	if !valid {
-		return nil, errors.New("wrong username or password")
-	}
-
-	// 3. OK
-	return &user, nil
-}
-
-func (r *UserRepository) GetAllUsers() ([]models.User, error) {
-	var users []models.User
-	result := r.db.Preload("Role").Find(&users)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return users, nil
+type IUserRepository interface {
+	FindByEmail(email string) (*models.User, error)
+	StoreUser(email, fullname, hashed_password, role string) error
+	CheckUser(email, password string) (*models.User, error)
+	GetAllUsers() ([]models.User, error)
 }
--- identity-service/internal/services/user_service.go
+++ identity-service/internal/services/user_service.go
@@ -1,62 +1,9 @@
 package services
 
-import (
-	"log"
-	"os"
-	"time"
+import "github.com/kchan139/intelligent-tutoring-system/identity-service/internal/models"
 
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/kchan139/intelligent-tutoring-system/identity-service/internal/models"
-	"github.com/kchan139/intelligent-tutoring-system/identity-service/internal/repositories"
-	"github.com/kchan139/intelligent-tutoring-system/identity-service/internal/utils"
-)
-
-type UserService struct {
-	repo repositories.IUserRepository
-}
-
-func NewUserService(repo repositories.IUserRepository) *UserService {
-	return &UserService{repo}
-}
-
-func (s *UserService) Authenticate(email, password string) (string, error) {
-	user, err := s.repo.CheckUser(email, password)
-	if err != nil {
-		log.Printf("error from checking user existence in userService: %v", err)
-		return "", err
-	}
-
-	// making jwt
-	claims := jwt.MapClaims{
-		"id":    user.ID,
-		"email": user.Email,
-		"role":  user.Role.RoleName,                    // or user.Role.RoleName if preloaded
-		"exp":   time.Now().Add(24 * time.Hour).Unix(), // expires in 24 hours
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	jwt_key := []byte(os.Getenv("JWT_KEY"))
-	signedToken, err := token.SignedString(jwt_key)
-	if err != nil {
-		log.Printf("error from generating token in userService: %v", err)
-		return "", err
-	}
-
-	return signedToken, nil
-}
-func (s *UserService) CreateUser(email, fullname, password, role string) error {
-	hash, err := utils.HashPassword(password)
-	if err != nil {
-		log.Printf("error from hashing password in userService: %v", err)
-		return err
-	}
-	err = s.repo.StoreUser(email, fullname, hash, role)
-	if err != nil {
-		log.Printf("error from storing new user in userService: %v", err)
-		return err
-	}
-	return nil
-}
-
-func (s *UserService) GetAllUsers() ([]models.User, error) {
-	return s.repo.GetAllUsers()
+type IUserService interface {
+	Authenticate(email, password string) (string, error)
+	CreateUser(email, fullname, password, role string) error
+	GetAllUsers() ([]models.User, error)
 }
